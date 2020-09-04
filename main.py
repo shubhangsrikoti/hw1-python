02b7d4085ab8f76a154b71f5fdee1be827a56e6e
Shubhang Srikoti <70000106+shubhangsrikoti@users.noreply.github.com>
2020-09-04 22:31:14 +0000

    removed lower case inputs

--- main.py
+++ main.py
@@ -18,28 +18,28 @@ gradepoint1 = 0
 gradepoint2 = 0
 gradepoint3 = 0
 
-if courseletter == "A" or courseletter == "a":
+if courseletter == "A":
   print("Grade point for course 1 is:",A)
   gradepoint1 =+ A
-elif courseletter == "A-" or courseletter == "a-":
+elif courseletter == "A-":
   print("Grade point for course 1 is:",Aminus)
   gradepoint1 =+ Aminus
-elif courseletter == "B+" or courseletter == "b+": 
+elif courseletter == "B+": 
   print("Grade point for course 1 is:",Bplus)
   gradepoint1 =+ Bplus
-elif courseletter == "B" or courseletter == "b":
+elif courseletter == "B":
   print("Grade point for course 1 is:",B)
   gradepoint1 =+ B
-elif courseletter == "B-" or courseletter == "b-":
+elif courseletter == "B-":
   print("Grade point for course 1 is:",Bminus)
   gradepoint1 =+ Bminus
-elif courseletter == "C+" or courseletter == "c+":
+elif courseletter == "C+":
   print("Grade point for course 1 is:",Cplus)
   gradepoint1 =+ Cplus
-elif courseletter == "C" or courseletter == "c":
+elif courseletter == "C":
   print("Grade point for course 1 is:",C)
   gradepoint1 =+ C
-elif courseletter == "D" or courseletter == "d":
+elif courseletter == "D":
   print("Grade point for course 1 is:",D)
   gradepoint1 =+ D
 else:
@@ -49,28 +49,28 @@ else:
 courseletter = input("Enter your course 2 letter grade: ")
 course2credit = float(input("Enter your course 2 credit: "))
 
-if courseletter == "A" or courseletter == "a":
+if courseletter == "A":
   print("Grade point for course 2 is:", A)
   gradepoint2 =+ A
-elif courseletter == "A-" or courseletter == "a-":
+elif courseletter == "A-":
   print("Grade point for course 2 is:",Aminus)
   gradepoint2 =+ Aminus
-elif courseletter == "B+" or courseletter == "b+": 
+elif courseletter == "B+": 
   print("Grade point for course 2 is:",Bplus)
   gradepoint2 =+ Bplus
-elif courseletter == "B" or courseletter == "b":
+elif courseletter == "B":
   print("Grade point for course 2 is:",B)
   gradepoint2 =+ B
-elif courseletter == "B-" or courseletter == "b-":
+elif courseletter == "B-":
   print("Grade point for course 2 is:",Bminus)
   gradepoint2 =+ Bminus
-elif courseletter == "C+" or courseletter == "c+":
+elif courseletter == "C+":
   print("Grade point for course 2 is:",Cplus)
   gradepoint2 =+ Cplus
 elif courseletter == "C" or courseletter == "c":
   print("Grade point for course 2 is:",C)
   gradepoint2 =+ C
-elif courseletter == "D" or courseletter == "d":
+elif courseletter == "D":
   print("Grade point for course 2 is:",D)
   gradepoint2 =+ D
 else:
@@ -80,28 +80,28 @@ else:
 courseletter = input("Enter your course 3 letter grade: ")
 course3credit = float(input("Enter your course 3 credit: "))
 
-if courseletter == "A" or courseletter == "a":
+if courseletter == "A":
   print("Grade point for course 3 is:",A)
   gradepoint3 =+ A
-elif courseletter == "A-" or courseletter == "a-":
+elif courseletter == "A-":
   print("Grade point for course 3 is:",Aminus)
   gradepoint3 =+ Aminus
-elif courseletter == "B+" or courseletter == "b+": 
+elif courseletter == "B+": 
   print("Grade point for course 3 is:",Bplus)
   gradepoint3 =+ Bplus
-elif courseletter == "B" or courseletter == "b":
+elif courseletter == "B":
   print("Grade point in course 3 is:",B)
   gradepoint3 =+ B
-elif courseletter == "B-" or courseletter == "b-":
+elif courseletter == "B-":
   print("Grade point for course 3 is:",Bminus)
   gradepoint3 =+ Bminus
-elif courseletter == "C+" or courseletter == "c+":
+elif courseletter == "C+":
   print("Grade point for course 3 is:",Cplus)
   gradepoint3 =+ Cplus
-elif courseletter == "C" or courseletter == "c":
+elif courseletter == "C":
   print("Grade point for course 3 is:",C)
   gradepoint3 =+ C
-elif courseletter == "D" or courseletter == "d":
+elif courseletter == "D":
   print("Grade point for course 3 is:",D)
   gradepoint3 =+ D
 else:
